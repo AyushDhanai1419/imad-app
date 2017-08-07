e819210b02362766c3f92d66ebc0efa72b5c3e9b
Ayush Dhanai <30471382+AyushDhanai1419@users.noreply.github.com>
2017-08-07 21:51:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,32 +10,20 @@ element.innerHTML = "New Value";
 //move the image on click
 
 var img = document.getElementById('madi');
-var marginLeft = 0,c=0;
+var marginLeft = 0;
 function moveRight(){
    
     marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + "px";
-    if(marginLeft >= 500)
-    {
-        c=1;
-    }
+    
 }
 
-function moveLeft(){
-   
-    marginLeft = marginLeft - 5;
-    img.style.marginLeft = marginLeft + "px";
-    if(marginLeft ===0)
-    {
-        c=0;
-    }
-}
+
 
 img.onclick = function(){
-    if(marginLeft <=500 && c===0)
+    
     var Rinterval = setInterval(moveRight,50);
-    else
-    var Linterval = setInterval(moveLeft,50);
+    
 
     
 };
