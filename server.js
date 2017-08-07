8553313a49a9c81e4fea8aaa3853fd1c3de70f90
Ayush Dhanai <30471382+AyushDhanai1419@users.noreply.github.com>
2017-08-07 09:32:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,70 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles ={
+'article-one' : {
+    title: 'Article One | Ayush Dhanai',
+    heading: 'Article One',
+    date: '15 Sep',
+    content: ` <p>This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.</p>
+        
+        <p>This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.</p>
+        
+        <p>This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.This is the content if Article one.</p>
+        `
+        },
+        'article-two' : {
+            title: 'Article Two | Ayush Dhanai',
+    heading: 'Article Two',
+    date: '10 Sep',
+    content: ` <p>This is the content if Article Two.This is the content if Article Two.</p>
+        `
+        },
+        
+        'article-three' : {
+            title: 'Article Three | Ayush Dhanai',
+    heading: 'Article Three',
+    date: '20 Sep',
+    content: ` <p>This is the content if Article Three.</p>
+        `
+        }
+};
+function createTemplate (data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmlTemplate=`
+    <html>
+   <head> 
+      <title>
+       ${title}
+      </title>
+      <meta name="viewport" content="width-device-width, initial-scale-1"/>
+              <link href="/ui/style.css" rel="stylesheet" />
+
+      
+    </head>
+    <body>
+        <div class="container">
+        <div><a href="/">HOME</a>
+        </div>
+        <div class="center">
+            <h1><u>${heading}</u></h1>
+        </div>
+        <div>
+            <h1><u>${date}</u></h1>
+        </div>
+        <div>
+       ${content}
+        </div> 
+         </div> 
+    </body>
+</html>
+    
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,17 +77,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one',function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName',function (req, res){
+    var articleName = rec.params.articleName;
+  res.sendFile(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
