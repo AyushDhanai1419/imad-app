80fa4f36aac385728e9c63cc603f060dd48a76b3
Ayush Dhanai <30471382+AyushDhanai1419@users.noreply.github.com>
2017-08-07 21:21:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,16 @@ alert("Hi I am Java Script");
 var element = document.getElementById('main-text');
 
 element.innerHTML = "New Value";
+
+//move the image on click
+
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + "px";
+}
+
+img.onClick = function(){
+    var interval = setInterval(moveRight,100);
+};
