a8395ba24697bd88c2e3aabac47da91119c678a6
Ayush Dhanai <30471382+AyushDhanai1419@users.noreply.github.com>
2017-08-07 21:08:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ alert("Hi I am Java Script");
 
 //change text of the main-text div
 
-var element = document.getElementById(main-text);
+var element = document.getElementById('main-text');
 
 element.innerHTML = "New Value";
