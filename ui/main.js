3bf8140aba16ecd5e403cfd9bb137d2af2619e41
Ayush Dhanai <30471382+AyushDhanai1419@users.noreply.github.com>
2017-08-07 22:32:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,3 @@
 console.log('Loaded!');
 
 
-//move the image on click
-
-var img = document.getElementById('ayush');
-var marginLeft = 0;
-function moveRight(){
-   
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + "px";
-    
-}
-
-
-
-img.onclick = function(){
-    
-    var interval = setInterval(moveRight,50);
-};
