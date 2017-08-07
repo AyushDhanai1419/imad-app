2634f056739db83c3514656c643d0da4452828f5
Ayush Dhanai <30471382+AyushDhanai1419@users.noreply.github.com>
2017-08-07 23:16:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,38 +69,39 @@ var htmlTemplate=`
 `;
 return htmlTemplate;
 }
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/:articleName',function (req, res){
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
-
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/counter', function (req, res) {
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-            counter = counter + 1;
-});
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+        app.get('/', function (req, res) {
+          res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+        });
+        
+        app.get('/ui/style.css', function (req, res) {
+          res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+        });
+        
+        app.get('/ui/main.js', function (req, res) {
+          res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+        });
+        
+        app.get('/:articleName',function (req, res){
+            var articleName = req.params.articleName;
+          res.send(createTemplate(articles[articleName]));
+        });
+        
+        
+        app.get('/ui/madi.png', function (req, res) {
+          res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+        });
+        
+        var counter = 0;
+        app.get('/counter', function (req, res) {
+                    
+                    counter = counter + 1;
+                    res.send(counter.toSring());
+        });
+        
+        // Do not change port, otherwise your app won't run on IMAD servers
+        // Use 8080 only for local development if you already have apache running on 80
+        
+        var port = 80;
+        app.listen(port, function () {
+          console.log(`IMAD course app listening on port ${port}!`);
+        });
