60dd7546d02bc3a7dc62b58133694740d6703800
Ayush Dhanai <30471382+AyushDhanai1419@users.noreply.github.com>
2017-08-09 00:33:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-/*
+
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function()
@@ -36,4 +36,3 @@ button.onclick = function()
  
   
 };
-*/
